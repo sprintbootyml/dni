1742d9e829a02703e7cd18da93df0204b6a1026c
SprintBootYml <sprintbootyml@gmail.com>
2025-06-29 01:20:54 +0000

    ok

--- src/main/java/pe/edu/vallegrande/api_reniec/config/WebConfig.java
+++ src/main/java/pe/edu/vallegrande/api_reniec/config/WebConfig.java
@@ -1,17 +1,28 @@
 package pe.edu.vallegrande.api_reniec.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.config.CorsRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.reactive.CorsWebFilter;
+
+import java.util.List;
 
 @Configuration
-public class WebConfig implements WebFluxConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping
-                        ("/**") // Permitir todas las rutas
-                .allowedOrigins("*") // Permitir todas las orígenes (ajusta según tus necesidades)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Métodos permitidos
-                .allowedHeaders("*"); // Permitir todos los headers
+public class WebConfig {
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("https://sturdy-tribble-97xxggjrj64xfxq69-4200.app.github.dev"));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true); // Si necesitas enviar cookies o headers como Authorization
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return new CorsWebFilter(source);
     }
 }
